e0ca42b6c23f0812438053b2516077fae672bc73
deepakjoshi03 <deepakjoshi0312@gmail.com>
2025-02-24 23:10:38 +0530

    Conversions

---
+++ 03_ConversionsandOperstions.js
@@ -0,0 +1,24 @@
+/*let score=undefined
+
+console.log(typeof(score));
+let valueInNumber=Number(score)    //type conversion ie score changes from string to number
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber); //will give NaN as output which means not a number for "33abc" and undefined
+*/
+//Notes
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+/*let isLoggedIn=0    
+let BooleanIsloggedIn=Boolean(isLoggedIn)
+console.log(BooleanIsloggedIn) */
+// give false for "" and 0
+//gives true for "Deepak" and 1
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+
+
