40ce7325dd7fce8c2c9fc10313c91eb5d2c77b9f
deepakjoshi03 <deepakjoshi0312@gmail.com>
2025-02-26 18:46:46 +0530

    comparisons and summary

---
+++ 04_comparisons.js
@@ -0,0 +1,29 @@
+//some common operators
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2<1);
+
+//Problems come when you compare different datatypes
+//Example
+// console.log("2" > 1);//sometimes we do not get the predictable result
+// console.log("02" > 1);
+//**** Null Case******
+// console.log(null>0);//false
+// console.log(null==0);//false
+// console.log(null>=0);//true
+//****the reason is that an equality check ==  and comparisons >< >= <= work differently
+//Comparisons converts null to a number ,treating it as 0.
+//Thats why null>=0 is true and null>0 is false 
+
+//Cases of undefined
+// console.log(undefined>0);//false
+// console.log(undefined==0);//false
+// console.log(undefined>=0);//False
+
+// ===(Strict Check) It not only checks the values but also check its data type
+//Example
+console.log("2"===2);//False
+console.log("2"==2);//True
+
---
+++ 05_DatatypesSummary.js
@@ -0,0 +1,41 @@
+//Datatypes can be divided into two categories based on how data is stored in memory and how can we access that data
+//1.Primitive
+//2.Non Primitive or Reference type
+
+
+//1.Primitive-> These are called by value
+// 7 Types : String,Number,boolean,null,undefined,Symbol(to make any value unique),Bigint
+//Examples
+// const score=100
+// const scorevalue=100.3
+
+// const isLoggedIn=false
+// const outsidetemp=null
+// let userEmail;//Undefined
+const id= Symbol('123')
+const id2=Symbol('123')
+console.log(id===id2);//False
+
+
+
+
+//2.Non Primitive or Reference type
+// Types:Array,Objects,functions
+
+const heros=["Deepak","Shaktiman","Padman"] //array
+//Example of Object
+let myobj={
+    name:"Deepak",
+    age:22,
+}
+//Example of Function
+const myFunction=function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myFunction);//output will be object function 
+console.log(typeof null);//output will be object
+console.log(typeof id);//output will be symbol
+
+
